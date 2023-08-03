9f7e87f66c07dd9b06004c52a976f94b0f9c373d
rssllbnvj <russell_buenviaje@dlsl.edu.ph>
2023-08-03 13:21:15 +0800

    header commit

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,5 +2,7 @@ const toggleMenu = document.querySelector('.toggle__menu');
 const dropdownMenu = document.querySelector('.dropdownMenu');
 
 toggleMenu.addEventListener('click', () => {
-    dropdownMenu.classList.toggle('hidden');
+
+    dropdownMenu.classList.toggle('h-[22rem]');
+
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    mode: "jit",
     content: ["./dist/**/*.{html,js}"],
     theme: {
         extend: {
             colors: {
+                lgurey: "#9d9d9d",
                 gurey: "#2c2c32",
                 dark: "#25252b",
                 asul: "#0066b8",
                 light: "#ffffff",
+                asuldark: "#0055a0",
+            },
+            screens: {
+                'sm': '500px',
             },
         },
+    purge:{
+        enabled: true,
+        content: ["./dist/**/*.{html,js}"],
+    }
     },
     plugins: ["prettier-plugin-tailwindcss"],
 };
